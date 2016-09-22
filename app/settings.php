a0405bcf89b97fbbda12dfaa2e0bc215f69b6dc3
Tom Graham <mail@tomgraham.engineer>
2016-09-22 23:27:40 +0100

    Updated CS

--- app/settings.php
+++ app/settings.php
@@ -4,10 +4,10 @@ return [
     'settings' => [
         // Twig views
         'view' => [
-            'template_path' => __DIR__.'/../templates',
-            'twig' => [
-                'cache' => __DIR__.'/../var/cache/twig',
-                'debug' => true,
+            'template_path' => __DIR__ . '/../templates',
+            'twig'          => [
+                'cache'       => __DIR__ . '/../var/cache/twig',
+                'debug'       => true,
                 'auto_reload' => true,
             ],
         ],
--- src/AppHandler.php
+++ src/AppHandler.php
@@ -32,19 +32,19 @@ class AppHandler implements AppHandlerInterface
         // Render the template with some example customer data
         return $this->view->fetch('app.html.twig', [
             'subscription' => [
-                'amount' => 1500,
+                'amount'  => 1500,
                 'created' => new DateTime('2015-06-01T09:00:00+00:00'),
             ],
             'charges' => [
                 [
                     'reference' => '12345',
-                    'date' => new DateTime('2016-09-20T09:00:10+00:00'),
-                    'amount' => 1500,
+                    'date'      => new DateTime('2016-09-20T09:00:10+00:00'),
+                    'amount'    => 1500,
                 ],
                 [
                     'reference' => '12346',
-                    'date' => new DateTime('2016-08-20T09:12:33+00:00'),
-                    'amount' => 1500,
+                    'date'      => new DateTime('2016-08-20T09:12:33+00:00'),
+                    'amount'    => 1500,
                 ],
             ],
         ]);
